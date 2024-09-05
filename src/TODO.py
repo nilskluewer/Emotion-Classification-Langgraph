14328e7df149a88587baf1732b4ef9cccb947de1
Nils Klüwer <n_klue@protonmail.com>
2024-09-05 11:49:10 +0200

    exldraw first pipleine scratch

---
+++ src/TODO.py
@@ -0,0 +1,3 @@
+# TODO: Prompt herausfinden für Lisa Feldmann Barrett
+# TODO: Langchain ansatz implementieren bei dem reasoning im textfeld und die ausgabe als tool call gemacht wird.
+# TODO: Cohere, OpenAi, Anthropic, Google anbinden
