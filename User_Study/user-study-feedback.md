f341db07602c35ce07f043e4e5294e3f84c0a7cc
Nils Klüwer <n_klue@protonmail.com>
2024-12-07 10:49:37 +0100

    Aspec adation

---
+++ User_Study/user-study-feedback.md
@@ -0,0 +1,12 @@
+- Einleitung fehlt
+- Benutzername hervorheben
+- Du liest das jetzt durch danach gibt es fragen dazu. 
+- Es ist nich tklar was geneiert ist? (Dh. punkt IV ist vom LLM generiert?)
+- Jan hat beim ersten durchgang : 6:30 Minuten gebraucht
+- 1 Stk. pro Nutzer -> 100 verschiedene User Analysis oder für 100 Nutzer die gleiche? 
+1 stk = hohe zuverlässig -> könnte sein das im falschen bnreich liegt
+100 verschiedene -> hohe validity aber hohe streung
+
+10 verschiedene auf 100 Personen.
+
+
--- src_llm_pipeline/utils/eval_aspects.py
+++ src_llm_pipeline/utils/eval_aspects.py
@@ -130,6 +130,16 @@ def aspect_evaluator(step_1_classification, step_2_classification_summary, aspec
     )
     return response
 
+def hallucination_evaluator():
+    messages = [
+        {"role": "system", "content": {f"""Please act as an impartial judge and evaluate the quality of the response provided by an AI assistant to the user question displayed below. Your evaluation should consider factors such as the helpfulness, relevance, accuracy, depth, creativity, and level of detail of the response. Begin your evaluation by providing a short explanation. Be as objective as possible. After providing your explanation, please rate the response on a scale of 1 to 10 by strictly following this format: '[[rating]]', for example: 'Rating: [[5]].'"""},
+        {"role": "model", "content": f"{role_feedback_prompt}"},
+        {"role": "user", "content": f"{user_task_prompt}"},
+    ]
+    [System] 
+    
+    return None
+
 def aspect_evaluator_all_aspects(step_1_classification, step_2_classification_summary,llm_model_name : str, run_tree_parent_id):    
     for aspect in Aspect:
         aspect_evaluator(step_1_classification, step_2_classification_summary, aspect, llm_model_name, run_tree_parent_id, langsmith_extra={"tags": [f"{aspect}"]})
